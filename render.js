14662f969cdcf5b34e0db0d0e66be143fcf8ffc6
watsonso <uni0360@gmail.com>
2017-11-24 00:48:11 +0900

    make render.js

---
+++ render.js
@@ -0,0 +1,34 @@
+var canvas = document.getElementsByTagName('canvas')[0];
+var ctx = canvas.getContext('2d');
+var W = 300, H = 600;
+var BLOCK_W = W / COLS, BLOCK_H = H / ROWS;
+
+function drawBlock(x, y) {
+  ctx.fillStyle(BLOCK_W * x, BLOCK_H * y, BLOCK_W - 1, BLOCK_H - 1);
+  ctx.strokeStyle(BLOCK_W * x, BLOCK_H * y, BLOCK_W - 1, BLOCK_H - 1);
+}
+
+function render() {
+  ctx.clearRact(0, 0, W, H);
+  ctx.strokeStyle = 'black';
+
+  for (var x = 0; x < COLS; ++x) {
+    for (var y = 0; y < ROWS; ++y) {
+      if (board[y][x]) {
+        ctx.fillStyle = colors[board[y][x] - 1];
+        drawBlock(x,y);
+      }
+    }
+  }
+
+  for (var y = 0; y < 4; ++y) {
+    for (var x = 0; x < 4; ++x) {
+      if (current[y][x]) {
+        ctx.fillStyle = colors[current[y][x] - 1];
+        drawBlock(currentX + x, currentY + y);
+      }
+    }
+  }
+}
+
+setInterval(render, 30);
